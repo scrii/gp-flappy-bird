155cb5be405fd6c73cfa0630d3935bab77cfd569
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-03-16 16:32:53 +0300

    feat(core): calculate dt properly and process new events

--- game/src/game.py
+++ game/src/game.py
@@ -1,30 +1,46 @@
 import pygame
 from settings import *
-from scenes.GameScene import GameScene
+from scenes.game_scene import GameScene
+import time
 
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.current_scene = GameScene(self)
+        self.current_scene = GameScene()
         self.clock = pygame.time.Clock()
         self.run_condition = True
-    
+        self.is_paused = False
+
     def run(self):
+        prev_time = time.time()
         while self.run_condition:
+            self.clock.tick(FPS)
+            now_time = time.time()
+            dt = now_time - prev_time
+            prev_time = now_time
+
             self.process_events()
-            self.update()
-            self.draw()
+            if not self.is_paused:
+                self.update(dt)
+                self.draw()
 
     def process_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                raise SystemExit
-
-            self.current_scene.handle_event(event)
+                self.run_condition = False
+                break
+            elif event.type == pygame.VIDEORESIZE:
+                self.screen = pygame.display.set_mode(
+                    (event.w, event.h), 
+                    pygame.RESIZABLE
+                )
+                break
+            elif event.type == pygame.ACTIVEEVENT:
+                self.is_paused = (event.gain == 0)
+            self.current_scene.process_event(event)
     
-    def update(self):
-        self.current_scene.update(self.clock.tick(FPS) / 1000)
+    def update(self, dt):
+        self.current_scene.update(dt)
 
     def draw(self):
         self.current_scene.draw(self.screen)
