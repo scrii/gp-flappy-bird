7133192fd561e8ec8b717bd7731abb3e7fec3d29
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-03-16 16:28:22 +0300

    feat(game_scene): add bird and pipes generation

--- game/src/scenes/game_scene.py
+++ game/src/scenes/game_scene.py
@@ -1,22 +1,51 @@
 import pygame
 from settings import Color
-from entities.Bird import Bird
-from entities.GraphicsObject import GraphicsObject
+from entities.bird import Bird
+from entities.graphics_object import GraphicsObject
 from gp import GeneticProgramming
+from scenes.scene import Scene
+from entities.physical_object import PhysicalObject
+from tools.hitbox import Hitbox
+from tools.point import Point
+from settings import ASSETS_PATH, PIPES_HORIZONTAL_GAP, SCREEN_WIDTH, SCREEN_HEIGHT, BIRD_SIZE
+from entities.pipe import Pipe
+from entities.composite_pipe import CompositePipe
 
-class GameScene:
-    def __init__(self, game):
-        self.game = game
-        self.birds = GeneticProgramming.initialize_population()
-    
-    def handle_event(self, event):
-        pass
+class GameScene(Scene):
+    def __init__(self):
+        super().__init__()
+        self.player = Bird(Hitbox(BIRD_SIZE, BIRD_SIZE), pygame.image.load(ASSETS_PATH + 'images/bird.png'), Point(0, 0))
+        self.add_graphics_object(
+            self.player
+        )
+        composite_pipe = CompositePipe(
+            image=pygame.image.load(ASSETS_PATH + 'images/pipe.png')
+        )
+        self.add_graphics_object(composite_pipe.lower_pipe)
+        self.add_graphics_object(composite_pipe.upper_pipe)
+        self.pipes = [composite_pipe.lower_pipe, composite_pipe.upper_pipe]
+        self.birds = [self.player]
+
+    def process_event(self, event: pygame.event.Event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                self.player.jump()
 
     def update(self, dt):
-        for bird in self.birds:
-            bird.update()
+        super().update(dt)
+        if self.pipes and SCREEN_WIDTH - self.pipes[-1]._position.get_x() > PIPES_HORIZONTAL_GAP:
+            composite_pipe = CompositePipe(
+                image=pygame.image.load(ASSETS_PATH + 'images/pipe.png')
+            )
+            self.pipes.append(composite_pipe.lower_pipe)
+            self.pipes.append(composite_pipe.upper_pipe)
+            self.add_graphics_object(composite_pipe.lower_pipe)
+            self.add_graphics_object(composite_pipe.upper_pipe)
 
-    def draw(self, screen):
-        screen.fill(Color.WHITE.value)
-        for bird in self.birds:
-            bird.draw(screen)
+        for pipe in self.pipes:
+            if self.player.check_collision(pipe):
+                # Reset run_condition
+                
+                pass
+            if pipe.check_off_screen():
+                self.remove_graphics_object(self.pipes.pop(0))
