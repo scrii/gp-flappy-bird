5730f302ce1085f3bd225e11b18a2f61d6477316
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-03-16 16:35:27 +0300

    refactor: remove unnecessary functionality

--- game/src/gp.py
+++ game/src/gp.py
@@ -1,6 +1,6 @@
 import pygame.locals
-from entities.Bird import Bird
-from entities.GraphicsObject import GraphicsObject
+from entities.bird import Bird
+from entities.graphics_object import GraphicsObject
 from typing import List
 from settings import *
 import pygame
@@ -9,7 +9,3 @@ import pygame
 class GeneticProgramming:
     def __init__(self):
         pass
-
-    @staticmethod
-    def initialize_population() -> List[Bird]:
-        return [Bird((100,50), pygame.image.load(ASSETS_PATH + 'images/bird.png'), (0, 0))]
