3f6bc61b7a5c8c435d390ac6a129e2431cefe307
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-03-16 16:18:41 +0300

    refactor: utilize base classes

--- game/src/entities/Bird.py
+++ game/src/entities/Bird.py
@@ -1,10 +1,29 @@
-from game.src.entities.graphics_object import GraphicsObject
+from entities.physical_object import PhysicalObject
+from settings import JUMP_VELOCITY, FALL_ACCELERATION, BIRD_FALL_VELOCITY
 
-class Bird(GraphicsObject):
-    def update(self):
-        return super().update()
+class Bird(PhysicalObject):
+    def __init__(self, hitbox, image, position):
+        super().__init__(
+            hitbox=hitbox,
+            image=image,
+            position=position,
+            velocity_x=0,
+            velocity_y=BIRD_FALL_VELOCITY,
+            acceleration_x=0,
+            acceleration_y=0
+        )
+        self.jump_start = 0
+        self.is_jumped = False
     
-    def draw(self, screen):
-        return super().draw(screen)
-    
-    
+    def jump(self):
+        self.is_jumped = True
+        self.jump_start = self._position.get_y()
+        self.set_velocity_y(JUMP_VELOCITY)
+        self.set_acceleration_y(FALL_ACCELERATION)
+
+    def update(self, dt):
+        super().update(dt)
+        if self.is_jumped and self._position.get_y() > self.jump_start:
+            self.acceleration_y = 0
+            self.is_jumped = False
+            self.set_velocity_y(BIRD_FALL_VELOCITY)
