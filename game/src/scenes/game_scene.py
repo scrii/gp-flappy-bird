29a4482ccfafe00389d8b4e8ac53f75955802a8f
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-03-18 22:39:07 +0300

    feat(gameplay): process collisions

--- game/src/scenes/game_scene.py
+++ game/src/scenes/game_scene.py
@@ -14,17 +14,17 @@ from entities.composite_pipe import CompositePipe
 class GameScene(Scene):
     def __init__(self):
         super().__init__()
+        self.reached_pipes = 0
         self.player = Bird(Hitbox(BIRD_SIZE, BIRD_SIZE), pygame.image.load(ASSETS_PATH + 'images/bird.png'), Point(BIRD_X_POSITION, 0))
         self.add_graphics_object(
             self.player
         )
-        composite_pipe = CompositePipe(
-            image=pygame.image.load(ASSETS_PATH + 'images/pipe.png')
-        )
-        self.add_graphics_object(composite_pipe.lower_pipe)
-        self.add_graphics_object(composite_pipe.upper_pipe)
-        self.pipes = [composite_pipe.lower_pipe, composite_pipe.upper_pipe]
         self.birds = [self.player]
+        self.pipes = []
+        self.generate_composite_pipe()
+        self.next_pipe = self.pipes[0]
+        self.is_next_pipe_added = True
+
 
     def process_event(self, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
@@ -34,18 +34,43 @@ class GameScene(Scene):
     def update(self, dt):
         super().update(dt)
         if self.pipes and SCREEN_WIDTH - self.pipes[-1]._position.get_x() > PIPES_HORIZONTAL_GAP:
-            composite_pipe = CompositePipe(
-                image=pygame.image.load(ASSETS_PATH + 'images/pipe.png')
-            )
-            self.pipes.append(composite_pipe.lower_pipe)
-            self.pipes.append(composite_pipe.upper_pipe)
-            self.add_graphics_object(composite_pipe.lower_pipe)
-            self.add_graphics_object(composite_pipe.upper_pipe)
+            self.generate_composite_pipe()
+
+        if self.is_next_pipe_added and \
+            self.birds and self.next_pipe._position.get_x() - self.birds[0]._position.get_x() <= 0:
+                self.reached_pipes += 1
+                self.is_next_pipe_added = False
+                print("reached")
 
         for pipe in self.pipes:
-            if self.player.check_collision(pipe):
-                # Reset run_condition
-                
-                pass
             if pipe.check_off_screen():
                 self.remove_graphics_object(self.pipes.pop(0))
+            if not self.is_next_pipe_added and \
+                self.birds and pipe._position.get_x() - self.birds[0]._position.get_x() > 0:
+                self.next_pipe = pipe
+                self.is_next_pipe_added = True
+                print("pipe is switched")
+            
+        for bird in self.birds:
+            if bird.check_off_screen():
+                bird.stats.set_score(self.reached_pipes)
+                self.birds.remove(bird)
+                self.remove_graphics_object(bird)
+                print(bird.stats.get_score())
+
+        for bird in self.birds:
+            for pipe in self.pipes:
+                if bird.check_collision(pipe):
+                    bird.stats.set_score(self.reached_pipes)
+                    self.birds.remove(bird)
+                    self.remove_graphics_object(bird)
+
+    
+    def generate_composite_pipe(self):
+        composite_pipe = CompositePipe(
+                image=pygame.image.load(ASSETS_PATH + 'images/pipe.png')
+            )
+        self.pipes.append(composite_pipe.lower_pipe)
+        self.pipes.append(composite_pipe.upper_pipe)
+        self.add_graphics_object(composite_pipe.lower_pipe)
+        self.add_graphics_object(composite_pipe.upper_pipe)
